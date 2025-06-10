9dc252ff8e0c033b2b6a13537272aa4499ab2e93
a.maschietto <a.maschietto@donboscosandona.it>
2025-06-10 08:47:57 +0200

    fase 5

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,14 @@ class HomeController < ApplicationController
             OpenStruct.new(titolo: "Progetti Parchi", anno:"5 Parchi creati", immagine:"https://images.pexels.com/photos/158063/bellingrath-gardens-alabama-landscape-scenic-158063.jpeg"),       
         ]
     end
+
+    def progetti 
+        @carditems = [
+            OpenStruct.new(titolo: "Progetti Case", anno:"129 Case Progettate", immagine:"https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg"),
+            OpenStruct.new(titolo: "Progetti Importanti", anno:"12 Progetti Importanti", immagine:"https://images.pexels.com/photos/9716236/pexels-photo-9716236.jpeg"),
+            OpenStruct.new(titolo: "Progetti Parchi", anno:"5 Parchi creati", immagine:"https://images.pexels.com/photos/158063/bellingrath-gardens-alabama-landscape-scenic-158063.jpeg"),       
+        ]
+    end
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get '/home', to: 'home#index'
   get '/storia', to: 'home#storia'
   get '/lavora', to: 'home#lavora_con_noi'
+  get '/progetti', to: 'home#progetti'
+  get '/richiedi', to: 'home#richiedi'
   root 'home#index'
 end
